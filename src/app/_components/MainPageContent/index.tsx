5b40c2e6f161b0c30c3eaf05975487b2f7e08cbb
Gribanov_German <germangribanov@mail.ru>
2024-06-16 02:48:13 +0530

    feat: add styles to main banner desktop & mobile

--- src/app/_components/MainPageContent/index.tsx
+++ src/app/_components/MainPageContent/index.tsx
@@ -1,14 +1,13 @@
 import AdditionalInformation from "@/_components/MainPageContent/AdditionalInformation";
 import Benefits from "@/_components/MainPageContent/Benefits";
 import MainBanner from "@/_components/MainPageContent/MainBanner";
-import TagsFilter from "@/_components/MainPageContent/TagsFilter";
 
 const MainPageContent = () => {
   return (
     <>
       <MainBanner />
       <Benefits />
-      <TagsFilter />
+      {/*<TagsFilter />*/}
       <AdditionalInformation />
     </>
   );
