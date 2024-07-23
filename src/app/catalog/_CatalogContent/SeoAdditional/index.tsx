a1c6ac5ea9e6590c193eea46486b9fbb211d756f
Gribanov_German <germangribanov@mail.ru>
2024-07-23 16:58:10 +0530

    feat: add SeoAdditional & Recommendations

---
+++ src/app/catalog/_CatalogContent/SeoAdditional/index.tsx
@@ -0,0 +1,25 @@
+interface SeoAdditionalProps {
+  additionalInfo: { id: number; title: string; description: string }[];
+}
+
+const SeoAdditional = ({ additionalInfo }: SeoAdditionalProps) => {
+  return (
+    <div className="container flex justify-start md:justify-center">
+      <ul className="flex flex-col gap-2.5 lg:gap-3.5 max-w-[546px] md:max-w-[690px] lg:max-w-[895px] xl:max-w-full">
+        {additionalInfo.map(data => {
+          const { id, title, description } = data;
+          return (
+            <li key={id}>
+              <h3 className="mb-3.5 md:mb-5 text-2xl md:text-4xl font-medium">
+                {title}
+              </h3>
+              <p className="text-xs md:text-sm m-0 leading-5">{description}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default SeoAdditional;
