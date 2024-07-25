3140c445e3f95c30c98dd85a47786aa0a47c2776
Gribanov_German <germangribanov@mail.ru>
2024-07-25 16:16:47 +0530

    refactor: styles

--- src/app/catalog/_CatalogContent/index.tsx
+++ src/app/catalog/_CatalogContent/index.tsx
@@ -15,31 +15,34 @@ const CatalogContent = () => {
   const [isAsideVisible, setAsideVisible] = useState(false);
   return (
     <>
-      <div className="container">
-        <div className="pt-4 mb-2 md:mb-7">
-          <CustomBreadCrumb
-            routes={[
-              { id: 1, href: "/", title: "Главная" },
-              { id: 2, href: "/catalog", title: "Лодки ПВХ Altair" },
-            ]}
-          />
+      <section>
+        <div className="container">
+          <div className="pt-4 mb-2 md:mb-7">
+            <CustomBreadCrumb
+              routes={[
+                { id: 1, href: "/", title: "Главная" },
+                { id: 2, href: "/catalog", title: "Лодки ПВХ Altair" },
+              ]}
+            />
+          </div>
+          <div className="flex gap-10 mb-8">
+            <AsideMenu filters={MockFiltersAsideMenu} />
+            <AsideMobile
+              filters={MockFiltersAsideMenu}
+              isVisible={isAsideVisible}
+              setIsVisible={setAsideVisible}
+            />
+            <CatalogResults
+              isAsideVisible={isAsideVisible}
+              setAsideVisible={setAsideVisible}
+            />
+          </div>
         </div>
-        <div className="flex gap-10 mb-8">
-          <AsideMenu filters={MockFiltersAsideMenu} />
-          <AsideMobile
-            filters={MockFiltersAsideMenu}
-            isVisible={isAsideVisible}
-            setIsVisible={setAsideVisible}
-          />
-          <CatalogResults
-            isAsideVisible={isAsideVisible}
-            setAsideVisible={setAsideVisible}
-          />
-        </div>
-      </div>
+      </section>
       <VideoConsultation />
       <SeoAdditional additionalInfo={MockSeoData} />
       <Recommendations />
+      <div className="h-1" />
     </>
   );
 };
